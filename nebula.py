f119d6c6f5e6f6da8ab720c754b00b470726a6dc
Kishore Venkateshan <kishorevenkateshan@Kishores-MacBook-Pro.local>
2018-09-30 02:17:38 -0700

    Added a way to track transition possiblities

--- nebula.py
+++ nebula.py
@@ -14,6 +14,34 @@ def print_state(state):
     sys.stdout.write('\n\n')
 
 
+class TransitionNode:
+    def __init__(self, config):
+        self.config = config
+        self.possibleAssoc = []
+
+    def get_new_possibilities(self, config_so_far):
+        new_possibilities = []
+        for possibility in self.possibleAssoc:
+            if possibility & config_so_far == config_so_far:
+                new_possibilities.append(possibility)
+
+        return new_possibilities
+
+
+def build_transition_dictionary():
+    transition_dict = dict()
+
+    for possible_config in range(64):
+        possible_config_state = build_mini_state(possible_config)
+        next_state = build_next_state(possible_config_state)
+        next_config = get_num_from_state(next_state)
+        if next_config not in transition_dict:
+            transition_dict[next_config] = TransitionNode(next_config)
+        transition_dict[next_config].possibleAssoc.append(possible_config)
+
+    return transition_dict
+
+
 def build_state(width, height):
     return [[False]*width for _ in range(height)]
 
@@ -23,6 +51,28 @@ def is_gas_present(cur_state, grid_row, grid_col):
                      int(cur_state[grid_row + 1][grid_col]) + int(cur_state[grid_row + 1][grid_col + 1]))
 
     return 1 == num_gas_moles
+
+
+def build_mini_state(num):
+    state = [
+        [num & 32 != 0, num & 16 != 0, num & 8 != 0],
+        [num & 4 != 0, num & 2 != 0, num & 1 != 0]
+    ]
+
+    print_state(state)
+
+    return state
+
+
+def get_num_from_state(state):
+    width = len(state[0])
+    height = len(state)
+    state_string = ''
+    for grid_row in range(height):
+        for grid_col in range(width):
+            state_string += str(int(state[grid_row][grid_col]))
+
+    return int(state_string, 2)
 
 
 def build_next_state(cur_state):
@@ -58,10 +108,14 @@ def random_state_generator(width, height):
     return state
 
 
+build_transition_dictionary()
+
 #state_1 = [[False, True, False, False], [False, False, True, False], [False, False, False, True], [True, False, False, False]]
 #print_state(state_1)
 #print_state(build_next_state(state_1))
 
-state = random_state_generator(5, 6)
-print_state(state)
-print_state(build_next_state(state))
+#state = random_state_generator(5, 6)
+#print_state(state)
+#print_state(build_next_state(state))
+
+#build_mini_state(num=33)

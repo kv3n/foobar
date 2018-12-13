974d2317992a064b9c1a0f5127eaccd7dd165000
Kishore Venkateshan <kishorevenkateshan@Kishores-MacBook-Pro.local>
2018-12-13 11:47:29 +0530

    Minor adjustment to aocread

--- aocread.py
+++ aocread.py
@@ -1,5 +1,6 @@
 def read_file(input_file):
     with open('aoc_input/' + input_file + '.txt', 'r') as fp:
         all_input = fp.readlines()
+        all_input = [input_line.rstrip('\n') for input_line in all_input]
 
     return all_input
